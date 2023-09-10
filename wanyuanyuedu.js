336cee70eba47d876545f1153804da9a31f1f3aa
zhengjianyang <71212291+zhengjianyang@users.noreply.github.com>
2023-09-10 21:04:57 +0800

    Update wanyuanyuedu.js

--- wanyuanyuedu.js
+++ wanyuanyuedu.js
@@ -3,7 +3,7 @@
 万源阅读：https://apps.apple.com/app/id6446446808
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/zhengjianyang/ADRule/main/wanyuanyuedu.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/zhengjianyang/SurgeRule/main/wanyuanyuedu.js
 
 [MITM]
 hostname = buy.itunes.apple.com
