30582a88469b124820cdda9524693045ffa15ede
zhengjianyang <71212291+zhengjianyang@users.noreply.github.com>
2023-09-20 01:18:38 +0800

    Update wanyuanyuedu.js

--- wanyuanyuedu.js
+++ wanyuanyuedu.js
@@ -24,7 +24,7 @@ if (result.receipt.bundle_id != "com.lmf.wext") {
 }
 let receipt = {
   "quantity": "1",
-  "product_id": "com.lmf.wext.year",
+  "product_id": "com.lmf.wext.month",
   "transaction_id": "70001829429789",
   "original_transaction_id": "70001829429789",
   "purchase_date": "2023-09-19 17:01:30 Etc/GMT",
@@ -33,18 +33,18 @@ let receipt = {
   "original_purchase_date": "2023-09-19 17:01:31 Etc/GMT",
   "original_purchase_date_ms": "1695142891000",
   "original_purchase_date_pst": "2023-09-19 10:01:31 America/Los_Angeles",
-  "expires_date": "2099-09-22 17:01:30 Etc/GMT",
-  "expires_date_ms": "4093750890000",
-  "expires_date_pst": "2099-09-22 10:01:30 America/Los_Angeles",
+  "expires_date": "2023-09-22 17:01:30 Etc/GMT",
+  "expires_date_ms": "1695402090000",
+  "expires_date_pst": "2023-09-22 10:01:30 America/Los_Angeles",
   "web_order_line_item_id": "70000841283507",
-  "is_trial_period": "false",
+  "is_trial_period": "true",
   "is_in_intro_offer_period": "false",
   "in_app_ownership_type": "PURCHASED",
   "subscription_group_identifier": "21355328"
 }
 let renewal = {
-  "auto_renew_product_id": "com.lmf.wext.year",
-  "product_id": "com.lmf.wext.year",
+  "auto_renew_product_id": "com.lmf.wext.month",
+  "product_id": "com.lmf.wext.month",
   "original_transaction_id": "70001829429789",
   "auto_renew_status": "1"
 }
