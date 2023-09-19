c5f2cb02c80561b8cf662959453265e186634fd9
zhengjianyang <71212291+zhengjianyang@users.noreply.github.com>
2023-09-20 00:58:11 +0800

    Update wanyuanyuedu.js

--- wanyuanyuedu.js
+++ wanyuanyuedu.js
@@ -23,10 +23,15 @@ if (result.receipt.bundle_id != "com.lmf.wext") {
   $done({});
 }
 let receipt = {
+  "receipt_type": "Production",
   "quantity": "1",
+  "app_item_id": 6446446808,
+  "adam_id": 6446446808,
+  "receipt_creation_date": "2023-09-19 07:10:41 Etc\/GMT",
+  "receipt_creation_date_pst": "2023-09-19 00:10:41 America\/Los_Angeles",
   "purchase_date_ms": "1686002766000",
-  "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
-  "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
+  "expires_date": "2099-06-06 06:06:06 Etc\/GMT",
+  "expires_date_pst": "2099-06-06 06:06:06 America\/Los_Angeles",
   "is_in_intro_offer_period": "false",
   "transaction_id": "666666666666666",
   "is_trial_period": "false",
@@ -38,9 +43,9 @@ let receipt = {
   "subscription_group_identifier": "20877951",
   "original_purchase_date_ms": "1686002766000",
   "web_order_line_item_id": "666666666666666",
-  "expires_date_ms": "148204937166000",
+  "expires_date_ms": "408438036600000",
   "purchase_date_pst": "2023-06-06 06:06:06 America\/Los_Angeles",
-  "original_purchase_date": "2023-06-06 06:06:06 Etc\/GMT"
+  "original_purchase_date": "2023-09-09 10:04:20 Etc\/GMT"
 }
 let renewal = {
   "expiration_intent": "1",
